12394fab4fb6216e2e61cb113ff843874a0727e2
djmorosini <djmorosini@outlook.com>
2018-12-05 12:18:57 -0500

    opening connection to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const dbUrl = process.env.MONGO_URI || process.env.MONGOLAB_MAUVE_URI || "mongodb://localhost:27017/startup-vermont"
+
+
+const MongoClient = require('mongodb').MongoClient;
+async function connect(dbUrl) {
+  console.log(`Connecting to ${dbUrl}...`)
+  let dbClient = await MongoClient.connect(dbUrl, { useNewUrlParser: true })
+  console.log("Connected to database.");
+  console.log(dbClient.isConnected())
+}
+connect(dbUrl);
+
 
 app.use(express.static('build'))
 
-app.get('/test', (request, response) => response.send('Hello World!'))
+app.get('/test', (request, response) => {
+  response.send(process.env.TEST)
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
