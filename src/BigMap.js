bf15efa0105e5c4ae5e668391bd2f5a6e9c1ae88
Christopher Leow <chrisjleow@gmail.com>
2018-12-12 17:09:31 -0500

    refactoring and got rid of map pop ups

--- src/BigMap.js
+++ src/BigMap.js
@@ -1,74 +1,86 @@
-import React, { Component, Fragment } from 'react';
-import { Map, Marker, Popup, TileLayer, GeoJSON, ZoomControl } from 'react-leaflet'
-import './Map.css';
-import L from 'leaflet';
+import React, { Component, Fragment } from "react";
+import {
+  Map,
+  Marker,
+  Popup,
+  TileLayer,
+  GeoJSON,
+  ZoomControl
+} from "react-leaflet";
+import "./Map.css";
+import L from "leaflet";
 import vermonts_border from "./border.js";
 
 class StartupsMap extends Component {
   constructor(props) {
     super(props);
     this.center = [44, -72.7317];
-    this.zoomLevel = 7.5
-    this.startups = []
-    this.state = {
-      markers: []
-    }
+    this.zoomLevel = 7.5;
+    this.state = { startups: [] };
   }
 
-  addMarkers = () => {
-    let allstartups = this.startups
-    let markers = []
-    allstartups.forEach((startup, index) => {
-      console.log('adding marker')
-      console.log(startup)
-      if (startup.latlong !== null) {
-        let latlong = startup.latlong
-        markers.push({ key: `marker${index}`, position: [latlong], content: startup.name })
-      }
-    })
-    this.setState({ markers: markers })
-  }
-  
   componentWillReceiveProps(nextProps) {
-    this.startups = nextProps.startups
-    this.addMarkers()
+    this.setState({ startups: nextProps.startups });
   }
 
+  markerClicked(event) {
+    console.log({event});
+  }
 
   render() {
-    const center = this.center
-    let polygon = <GeoJSON data={vermonts_border} style={{ fillColor: '#048229', fillOpacity: '0.0005', color: '#048229' }} />
+    const center = this.center;
+    let polygon = (
+      <GeoJSON
+        data={vermonts_border}
+        style={{
+          fillColor: "#048229",
+          fillOpacity: "0.0005",
+          color: "#048229"
+        }}
+      />
+    );
 
     const myIcon = L.icon({
-      iconUrl: require('./Map_pin_icon.svg'),
+      iconUrl: require("./Map_pin_icon.svg"),
       iconSize: [15, 20],
       iconAnchor: [0, 20],
       popupAnchor: [8, -20],
       shadowUrl: null,
       shadowSize: null,
       shadowAnchor: null,
-      className: 'myMarkers'
-  });
+      className: "myMarkers"
+    });
 
     return (
-      <Map id='map' ref={this.mapRef} center={center} zoom={this.zoomLevel} zoomControl={false}>
+      <Map
+        id="map"
+        ref={this.mapRef}
+        center={center}
+        zoom={this.zoomLevel}
+        zoomControl={false}
+      >
         <TileLayer
           attribution='Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url='https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png'
+          url="https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
         />
         <ZoomControl position="bottomright" />
         {polygon}
         <Fragment>
-          {this.state.markers.map(marker => {
-            return (
-              <Marker icon={myIcon} key={marker.key} position={marker.position[0]}>
-                <Popup className="pop-ups">{marker.content}</Popup>
-              </Marker>
-            )
+          {this.state.startups.map(startup => {
+            if (startup.latlong) {
+              return (
+                <Marker
+                  icon={myIcon}
+                  key={startup.slug}
+                  position={startup.latlong}
+                  onClick={this.markerClicked}
+                />
+              );
+            }
           })}
         </Fragment>
       </Map>
-    )
+    );
   }
 }
 
