d0797fcac01f8afc0fa868b5a22d26e1e69f1e68
Kate <kate.voorhes@gmail.com>
2018-12-12 23:59:41 -0500

    search bar feature update

--- src/App.js
+++ src/App.js
@@ -138,7 +138,6 @@ class App extends Component {
     this.state.filteredStartups.forEach(startup => {
       if (startup.categories.includes(tagName)) {
         filtered.push(startup)
-        count++
       }
     });
     this.setState({ filter: tagName })
